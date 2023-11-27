876b5a1a97db452b1e1ffdb508b820abfa2e2f28
Hendrik <hendrik2002.hkk@gmail.com>
2023-11-26 23:10:13 -0300

    melhoria nos exemplos

--- app/src/main/java/com/example/parkpass/ListaDeEstacionamentos.java
+++ app/src/main/java/com/example/parkpass/ListaDeEstacionamentos.java
@@ -25,9 +25,14 @@ public class ListaDeEstacionamentos {
     }
 
     private void popularListaEstacionamento() {
-        // Exemplo de adição de estacionamentos fictícios
-        adicionarEstacionamento(new Estacionamento(102, "Estacionamento 1", "123456789", "email1@example.com", "123456789", new ListaDeVagas(), 4.9f, 15.50f, "08:00 - 18:00"));
-        adicionarEstacionamento(new Estacionamento(208, "Estacionamento 2", "987654321", "email2@example.com", "987654321", new ListaDeVagas(), 4.2f, 9.9f, "10:00 - 22:30"));
+        adicionarEstacionamento(new Estacionamento(101, "Estacionamento VIP", "123456789", "contato@estacionamentovip.com", "987654321", new ListaDeVagas(), 4.9f, 25.00f, "07:00 - 22:00"));
+        adicionarEstacionamento(new Estacionamento(202, "Estacionamento Central", "987654321", "contato@estacionamentocentral.com", "123456789", new ListaDeVagas(), 4.5f, 18.50f, "09:00 - 20:00"));
+        adicionarEstacionamento(new Estacionamento(303, "Estacionamento Rápido e Seguro", "234567890", "contato@estacionamentorapido.com", "987654321", new ListaDeVagas(), 4.7f, 15.75f, "08:30 - 18:30"));
+        adicionarEstacionamento(new Estacionamento(404, "Estacionamento Amigo do Ambiente", "345678901", "contato@amigoambiental.com", "234567890", new ListaDeVagas(), 4.2f, 12.99f, "10:00 - 21:00"));
+        adicionarEstacionamento(new Estacionamento(505, "Estacionamento Noturno", "456789012", "contato@estacionamentonoturno.com", "345678901", new ListaDeVagas(), 3.8f, 22.50f, "18:00 - 03:00"));
+        adicionarEstacionamento(new Estacionamento(606, "Estacionamento Familiar", "567890123", "contato@estacionamentofamiliar.com", "456789012", new ListaDeVagas(), 4.6f, 20.00f, "09:00 - 19:00"));
+        adicionarEstacionamento(new Estacionamento(707, "Estacionamento Expresso", "678901234", "contato@estacionamentoexpresso.com", "567890123", new ListaDeVagas(), 4.4f, 17.25f, "07:30 - 22:30"));
+        adicionarEstacionamento(new Estacionamento(808, "Estacionamento Seguro", "789012345", "contato@estacionamentoseguro.com", "678901234", new ListaDeVagas(), 4.1f, 14.00f, "11:00 - 20:00"));
     }
 
     public Estacionamento selecionaEstacionamento(int idEstacionamento) {
